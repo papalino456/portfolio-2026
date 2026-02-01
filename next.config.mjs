2dc51c1433eab8746c70de743bab3757bcaa7575
Sebastian Barrio <sebastian.barrio.b@gmail.com>
2026-02-01 13:56:20 -0600

    Fix: add basePath to next.config.mjs for GitHub Pages support

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // If your repo is papalino456.github.io, you don't need basePath.
-  // If it's papalino456.github.io/portfolio-2026, uncomment below:
-  // basePath: '/portfolio-2026',
+  basePath: '/portfolio-2026',
 };
 
 export default nextConfig;
